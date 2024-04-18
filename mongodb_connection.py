ef5fb8ddacea67f18746ec40381ec08b47f56723
Riley McNeill <mqt3uz@virginia.edu>
2024-04-18 21:34:35 +0000

    added error handling

--- mongodb_connection.py
+++ mongodb_connection.py
@@ -2,18 +2,24 @@ from pymongo import MongoClient
 import os
 import json
 
-MONGOPASS = os.getenv('MONGOPASS')
-uri = "mongodb+srv://mqt3uz:OxPXuRVOH9uO9wTh@cluster0.rmavpxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+MYMONGOPASS = os.getenv('MYMONGOPASS')
+uri = "mongodb+srv://mqt3uz:OxPXuRVOH9uO9wTh@cluster0.rmavpxm.mongodb.net/?retryWrites=true&w=majority&appName=DB_2"
+client = MongoClient(uri, username='mqt3uz', password=MYMONGOPASS, connectTimeoutMS=200, retryWrites=True)
 db = client.DP_2
 collection = db.DP2
 
 def import_json_files(directory):
     for filename in os.listdir(directory):
         if filename.endswith(".json"):
-            file_path = os.path.join(directory, filename)
+            try:
+                file_path = os.path.join(directory, filename)
+            except Exception as e:
+                print(filename, e)
             with open(file_path) as file:
-                data = json.load(file)
+                try:
+                    data = json.load(file)
+                except Exception as e:
+                    print(file, e)
                 if isinstance(data, list):
                     collection.insert_many(data)
                 else:
